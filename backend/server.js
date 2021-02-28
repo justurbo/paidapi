5809bc1ccb27474f2b6c2f50450d77e0dd7fa624
JonasGG <jonas.gaigalas@ktu.edu>
2021-02-28 20:48:58 +0200

    Frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
-const port = 8000;
+const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Express here...')
 });
+
+app.use('/users/', require("./routes/usersRoute"));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
